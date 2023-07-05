b11dbe9265bdbc06d81fd2b27e61d3736c714a4f
Damilola Esan <47270092+ionikdev@users.noreply.github.com>
2023-07-05 21:50:55 +0100

    Feature: Unit Test Setup And Postman Integration

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,22 @@
+<?php
+namespace Tests\Unit;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase; // This will ensure your database is migrated and cleaned up after each test
+
+    /** @test */
+    public function a_user_can_be_created(): void
+    {
+        // Retrieve the user from the database
+        $user = User::all();
+
+        // Assert that the user was successfully retrieved from the database
+        $this->assertNotNull( $user);
+
+    }
+}
